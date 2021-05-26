7c0a64aea342a19bb9ec23281a635e1d0bda669d
ParkSeRyeong <63963540+ParkSeRyeong@users.noreply.github.com>
2021-05-26 18:37:04 +0900

    05.26 세령 memberDto 수정 + 회원가입, 탈퇴, 수정 완료

--- src/main/java/com/ssafy/happyhouse/controller/UserController.java
+++ src/main/java/com/ssafy/happyhouse/controller/UserController.java
@@ -13,11 +13,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -35,6 +39,8 @@ import io.swagger.annotations.ApiOperation;
 public class UserController {
 
 	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+	private static final String SUCCESS = "success";
+	private static final String FAIL = "fail";
 
 	@Autowired
 	private UserService userService;
@@ -50,7 +56,7 @@ public class UserController {
 		return mav;
 	}
 
-	@ApiOperation(value = "유저 로그인.", response = MemberDto.class)
+	@ApiOperation(value = "로그인.", response = MemberDto.class)
 	@PostMapping("/login")
 	public ResponseEntity<Map<String, String>> login(@RequestBody Map<String, String> loginInfo,
 			HttpServletResponse response) {
@@ -77,21 +83,21 @@ public class UserController {
 		}
 	}
 
-	@ApiOperation(value = "회원가입", response = MemberDto.class)
+	@ApiOperation(value = "회원가입", response = String.class)
 	@PostMapping("/regist")
-	public ResponseEntity<MemberDto> register(@RequestBody MemberDto memberDto) {
+	public ResponseEntity<String> register(@RequestBody MemberDto memberDto) {
 		try {
 			logger.debug("회원가입 : " + memberDto);
 			int n = userService.userRegister(memberDto);
 
 			if (n > 0) {
-				return new ResponseEntity<MemberDto>(memberDto, HttpStatus.OK);
+				return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
 			} else {
-				return new ResponseEntity<MemberDto>(memberDto, HttpStatus.NO_CONTENT);
+				return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
-			return new ResponseEntity<MemberDto>(memberDto, HttpStatus.NO_CONTENT);
+			return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
 		}
 	}
 
@@ -99,8 +105,43 @@ public class UserController {
 	@GetMapping("/logout")
 	public ResponseEntity<String> logout(HttpSession session) {
 		logger.debug("로그아웃");
-		// 로그아웃 기능 구현...jwt로...
 		return new ResponseEntity<String>("ok", HttpStatus.OK);
+	}
+
+	
+	@ApiOperation(value = "개인정보 수정", response = MemberDto.class)
+	@PutMapping(value = "/mypage")
+	public ResponseEntity<String> modify(@RequestBody MemberDto updateInfo) {
+		logger.debug("수정정보 : " + updateInfo);
+
+		try {
+			int n = userService.userModify(updateInfo);
+			if (n > 0) {
+				return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+			} else {
+				return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+		}
+	}
+
+	@ApiOperation(value = "회원 탈퇴", response = MemberDto.class)
+	@DeleteMapping("/")
+	public ResponseEntity<String> delete(@RequestParam String userid) {
+		logger.debug("회원 탈퇴 : 아이디 " + userid);
+		try {
+			int n = userService.userDelete(userid);
+			if (n > 0) {
+				return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+			} else {
+				return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+		}
 	}
 
 }
--- src/main/java/com/ssafy/happyhouse/model/dto/MemberDto.java
+++ src/main/java/com/ssafy/happyhouse/model/dto/MemberDto.java
@@ -2,157 +2,95 @@ package com.ssafy.happyhouse.model.dto;
 
 public class MemberDto {
 
-	String id;
-	String pw;
-	String name;
-	String address_sido;
-	String address_gugun;
-	String address_dong;
-	String phone;
-	String email;
-	String interest_sido;
-	String interest_gugun;
-	String interest_dong;
-
+	private String id;
+	private String pw;
+	private String name;
+	private String phone;
+	private String email;
+	private String postcode;
+	private String address;
+	private String extraAddress;
 
 	public MemberDto() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
-
-	public MemberDto(String id, String pw, String name, String address_sido, String address_gugun, String address_dong,
-			String phone, String email, String interest_sido, String interest_gugun, String interest_dong) {
+	public MemberDto(String id, String pw, String name, String phone, String email, String postcode, String address,
+			String extraAddress) {
 		super();
 		this.id = id;
 		this.pw = pw;
 		this.name = name;
-		this.address_sido = address_sido;
-		this.address_gugun = address_gugun;
-		this.address_dong = address_dong;
 		this.phone = phone;
 		this.email = email;
-		this.interest_sido = interest_sido;
-		this.interest_gugun = interest_gugun;
-		this.interest_dong = interest_dong;
+		this.postcode = postcode;
+		this.address = address;
+		this.extraAddress = extraAddress;
 	}
-
 
 	public String getId() {
 		return id;
 	}
 
-
 	public void setId(String id) {
 		this.id = id;
 	}
-
 
 	public String getPw() {
 		return pw;
 	}
 
-
 	public void setPw(String pw) {
 		this.pw = pw;
 	}
-
 
 	public String getName() {
 		return name;
 	}
 
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-
-	public String getAddress_sido() {
-		return address_sido;
-	}
-
-
-	public void setAddress_sido(String address_sido) {
-		this.address_sido = address_sido;
-	}
-
-
-	public String getAddress_gugun() {
-		return address_gugun;
-	}
-
-
-	public void setAddress_gugun(String address_gugun) {
-		this.address_gugun = address_gugun;
-	}
-
-
-	public String getAddress_dong() {
-		return address_dong;
-	}
-
-
-	public void setAddress_dong(String address_dong) {
-		this.address_dong = address_dong;
-	}
-
 
 	public String getPhone() {
 		return phone;
 	}
 
-
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-
 
 	public String getEmail() {
 		return email;
 	}
 
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
+	public String getPostcode() {
+		return postcode;
+	}
 
-	public String getInterest_sido() {
-		return interest_sido;
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getExtraAddress() {
+		return extraAddress;
+	}
+
+	public void setExtraAddress(String extraAddress) {
+		this.extraAddress = extraAddress;
 	}
 
 
-	public void setInterest_sido(String interest_sido) {
-		this.interest_sido = interest_sido;
-	}
-
-
-	public String getInterest_gugun() {
-		return interest_gugun;
-	}
-
-
-	public void setInterest_gugun(String interest_gugun) {
-		this.interest_gugun = interest_gugun;
-	}
-
-
-	public String getInterest_dong() {
-		return interest_dong;
-	}
-
-
-	public void setInterest_dong(String interest_dong) {
-		this.interest_dong = interest_dong;
-	}
-
-	
-	
-	
-	
-	
-	
-	
-	
 }
